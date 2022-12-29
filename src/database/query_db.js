2ca5644d929a66b1d3d3565f394505812824ac25
Shubham Pandey <shubhampandey@Shubhams-MacBook-Air.local>
2022-12-29 23:38:57 +0530

    added PUT, POST, DELETE methods

--- src/database/query_db.js
+++ src/database/query_db.js
@@ -1,12 +1,55 @@
 const { connectToDB } = require("./db_connection");
 
+var collection = null;
+
+async function getDBCollection() {
+  if (!collection) {
+    collection = await connectToDB();
+  }
+  return collection;
+}
+
 async function findStudent(student_id) {
-  const collection = await connectToDB();
-  console.log(student_id);
-  console.log(typeof student_id);
-  return collection.findOne({ registrationNo: student_id });
+  const db_collection = await getDBCollection();
+  return db_collection.findOne({ registrationNo: student_id });
+}
+
+async function findAllStudents() {
+  const db_collection = await getDBCollection();
+  return db_collection.find({});
+}
+
+async function insertStudent(body) {
+  const db_collection = await getDBCollection();
+  const student = db_collection.insertOne(body);
+  return student;
+}
+
+async function findStudentOnID(id) {
+  const db_collection = await getDBCollection();
+  return db_collection.findOne({ _id: id });
+}
+
+async function updateStudent(regId, body) {
+  const db_collection = await getDBCollection();
+  const student = db_collection.updateOne(
+    { registrationNo: regId },
+    { $set: body }
+  );
+  return student;
+}
+
+async function deleteStudent(regId) {
+  const db_collection = await getDBCollection();
+  const deleted = db_collection.deleteOne({ registrationNo: regId });
+  return deleted;
 }
 
 module.exports = {
   findStudent,
+  findAllStudents,
+  insertStudent,
+  findStudentOnID,
+  updateStudent,
+  deleteStudent,
 };
--- src/router/routing.js
+++ src/router/routing.js
@@ -1,7 +1,17 @@
 const express = require("express");
-const { findStudent } = require("../database/query_db");
+const {
+  findStudent,
+  findAllStudents,
+  insertStudent,
+  findStudentOnID,
+  updateStudent,
+  deleteStudent,
+} = require("../database/query_db");
+var bodyParser = require("body-parser");
 
 const router = express.Router();
+
+router.use(bodyParser.json());
 
 router.get("/:id", async (req, res) => {
   const regID = parseInt(req.params.id);
@@ -11,6 +21,31 @@ router.get("/:id", async (req, res) => {
   } else {
     res.json(students);
   }
+});
+
+router.post("/", async (req, res) => {
+  var student = await insertStudent(req.body);
+  student = await findStudentOnID(student.insertedId);
+  res.status(201).json(student);
+});
+
+router.put("/:id", async (req, res) => {
+  var student = await updateStudent(parseInt(req.params.id), req.body);
+  student = await findStudent(parseInt(req.params.id));
+  res.json(student);
+});
+
+router.delete("/:id", async (req, res) => {
+  var deleted = await deleteStudent(parseInt(req.params.id));
+  if (deleted.deletedCount == 1) {
+    res.status(204).send();
+  } else {
+    res.status(404).json({ error: "student not found!" });
+  }
+});
+
+router.get("/", async (req, res) => {
+  res.json(await findAllStudents());
 });
 
 module.exports = {
